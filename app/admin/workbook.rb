e7fd2999b55baad404344ccfe8ba0b899551a091
Prelang Builder <builder@prelang.com>
2015-11-24 19:14:42 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Workbook' to allow for create, update

--- app/admin/workbook.rb
+++ app/admin/workbook.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Workbook do
+  permit_params :title, :clientid, :memberid, :formid, :formver, :q001section, :q001qlong, :q001qshort, :q001qatext
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
